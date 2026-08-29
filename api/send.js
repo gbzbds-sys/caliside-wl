712a79f9676b82fe1420c003eb76acdb5342f567
gbzbds-sys <gbzbds@gmail.com>
2026-08-29 15:54:37 +0200

    Add files via upload

--- api/send.js
+++ api/send.js
@@ -1,3 +1,1 @@
-// Compatibilité avec les anciennes versions du frontend qui appellent encore /api/send.
-// Toutes les candidatures passent désormais par le même handler robuste.
 export { default } from './caliside-submit.js';
